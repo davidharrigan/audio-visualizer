230eeb5bf4fd192f72511da67cdc249dca811d4e
David Harrigan <dtk24@wildcats.unh.edu>
2014-01-22 00:17:51 -0500

    Delete shader.h

--- shader.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHADER_H_
-#define SHADER_H_
-
-#define BUFFER_OFFSET(offset) ((GLvoid*) (offset))
-
-GLuint loadShaders(const char * vertex_file_path,const char * fragment_file_path);
-
-#endif
